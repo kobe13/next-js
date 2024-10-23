49bc335cbc0e6f2e7ad6b39609dd06ad02bed57d
kobe13 <kobe13010@gmail.com>
2024-10-23 16:42:38 +0200

    tests update

--- test-app/cypress/component/pageHome.cy.tsx
+++ test-app/cypress/component/pageHome.cy.tsx
@@ -2,7 +2,12 @@ import React from "react";
 import Home from "../../src/app/page";
 
 describe("component test Homepage", () => {
-  it("renders", () => {
+  it("renders welcome message and image", () => {
     cy.mount(<Home />);
+
+    cy.get("h1").should("be.visible").should("have.text", "Hello World");
+    cy.get("img")
+      .should("be.visible")
+      .should("have.attr", "alt", "Next.js Logo");
   });
 });
--- test-app/cypress/e2e/pageHome.cy.tsx
+++ test-app/cypress/e2e/pageHome.cy.tsx
@@ -1,7 +1,10 @@
 describe("e2e test Homepage", () => {
-  it("should display welcome message", () => {
+  it("should display welcome message and image", () => {
     cy.visit(Cypress.config("baseUrl") as string);
 
-    cy.contains("Get started").should("be.visible");
+    cy.get("h1").should("be.visible").should("have.text", "Hello World");
+    cy.get("img")
+      .should("be.visible")
+      .should("have.attr", "alt", "Next.js Logo");
   });
 });
